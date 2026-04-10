1fcb394af8471592ec0233436a872ec42fb0d500
Jeff Lee <jeff.lee@kitware.com>
2026-04-10 14:05:28 -0400

    chore: remove debug script

--- server/check_serve.py
+++
@@ -1,7 +0,0 @@
-from trame.app import get_server
-s = get_server()
-import inspect
-print(inspect.signature(s.serve) if callable(s.serve) else type(s.serve))
-print(repr(s.serve))
-print()
-print(repr(s._www))
